6e786134a5fa941d4578c08160d45e85f570206d
junokili <junokili@hotmail.com>
2020-09-01 02:45:13 +0000

    basic contact form added, no action

--- app.py
+++ app.py
@@ -198,6 +198,11 @@ def delete_event(event_id):
     return redirect(url_for('get_events'))
 
 
+@app.route('/contact_us')
+def contact_us():
+    return render_template("contact_us.html")
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
