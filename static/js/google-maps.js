63be94788308d0048046e7f2954bd0161c2c4600
junokili <junokili@hotmail.com>
2020-09-13 15:47:09 +0000

    added info and footer deets to base.html, new images for events and admin events

---
+++ static/js/google-maps.js
@@ -0,0 +1,25 @@
+function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"),{
+                zoom: 12,
+                center: {
+                    lat: -7.25150,
+                    lng: 112.747825
+                }
+            });    
+
+        var labels = "ABCDEFGHIJKLMNOPQPRSTUVWXYZ";
+
+        var locations = [
+            {lat: -7.281236, lng: 112.692476},
+        ];
+
+        var markers = locations.map(function(location, i){
+            return new google.maps.Marker({
+                position: location,
+                label: labels[i % labels.length]
+            });
+            }); 
+
+            var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"});
+        }
