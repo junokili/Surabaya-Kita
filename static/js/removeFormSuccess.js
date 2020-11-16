6cf6b6dac2d649709441f132d1a162a467bb5416
junokili <junokili@hotmail.com>
2020-11-16 10:05:56 +0000

    added back button to email success script

--- static/js/removeFormSuccess.js
+++ static/js/removeFormSuccess.js
@@ -5,6 +5,13 @@ function removeFormSuccess() {
     var el1 = document.getElementById("contact-div");
     el1.parentNode.removeChild(el1);
     var el = "";
-    el += `<p class="sm-text center-align">Thanks! We've received your message and will be in touch shortly.</p>`;
+    el += `<p class="sm-text center-align">Thanks! We've received your message and will be in touch shortly.</p>
+        <div class="row all-events-button">
+      <div class="col s12 center-align">
+        <a href="{{ url_for('get_events') }}" class="btn-large light-green accent-2 all-events-button">
+          Back to all Events<i class="fas fa-calendar-alt right"></i>
+        </a>
+      </div>
+    </div>`;
       document.getElementById("email-success").innerHTML = el;
 }
